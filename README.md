665b64a55c25e9bf574e375e97c8e538704ffc73
Amitendra Kumar <amitendrasrivastava@gmail.com>
2025-04-18 16:20:50 +0000

    Initial commit with training script and dataset

---
+++ README.md
@@ -0,0 +1,3 @@
+# SageMaker MLOps Demo
+
+This repo contains training code for predicting abalone age using XGBoost.
---
+++ train.py
@@ -0,0 +1,16 @@
+# train.py
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("abalone.csv")
+df['Sex'] = df['Sex'].map({'M': 0, 'F': 1, 'I': 2})
+
+X = df.drop('Rings', axis=1)
+y = df['Rings']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+dtrain = xgb.DMatrix(X_train, label=y_train)
+params = {'objective': 'reg:squarederror'}
+model = xgb.train(params, dtrain, num_boost_round=100)
+model.save_model("model.bst")
